cfce87c0c7a7e3ac628a9192b9e86c70edfdbab1
Brian Pfeil <brian.pfeil@gmail.com>
2009-06-18 12:36:50 -0400

    app01 logic change

--- apps/app01/javascripts/application.js
+++ apps/app01/javascripts/application.js
@@ -1,3 +1,12 @@
-console.log('begin - app01');
-console.log('app01 logic loaded!');
-console.log('end - app01');
+// bail if we don't have our main lib
+if (typeof jQuery === 'undefined') {
+    alert('Custom application extension failed: jQuery not available');
+    return;
+}
+
+jQuery(function($) {
+    console.log('page loaded');
+    console.log('begin - app01');
+    console.log('app01 logic loaded!');
+    console.log('end - app01');    
+});
