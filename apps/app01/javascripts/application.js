c409d6c48173ce58b886c1f85e72ee5be53b3fbf
Brian Pfeil <brian.pfeil@gmail.com>
2009-06-18 12:33:14 -0400

    app01 logic change

--- apps/app01/javascripts/application.js
+++ apps/app01/javascripts/application.js
@@ -1,1 +1,3 @@
-console.log('app01 logic loaded');
+console.log('begin - app01');
+console.log('app01 logic loaded!');
+console.log('end - app01');
--- common/javascripts/application_integration_entry_point.js
+++ common/javascripts/application_integration_entry_point.js
@@ -16,17 +16,22 @@ jQuery.noConflict();
 // page finished loading hook
 jQuery(function($) {
 
+    var appURL = function(relativePath) {
+      var basePath = 'http://github.com/pfeilbr/oracle-crm-ondemand-extension/raw/master';
+      return basePath + relativePath;
+    };
+
     // used to prevent browser caching of .js file with main application logic
     var randomNumber = Math.floor( Math.random() * 999999 );
     
     // src in main application logic
-    var applicationSourceURL = 'http://ajax.googleapis.com/ajax/libs/ext-core/3.0.0/ext-core.js';
+    //var applicationSourceURL = 'http://ajax.googleapis.com/ajax/libs/ext-core/3.0.0/ext-core.js';
+    var applicationSourceURL = appURL('/apps/app01/javascripts/application.js');
     var nonCacheableApplicationSourceURL = applicationSourceURL + '?' + randomNumber;
   
     // load app src dynamically to avoid browser caching  
     $.getScript(nonCacheableApplicationSourceURL, function() {
-        console.log('loaded: ' + nonCacheableApplicationSourceURL);
-        console.log('Ext = ' + Ext);
+        console.log('finished loading: ' + nonCacheableApplicationSourceURL);
     });
 
     // global injection test
