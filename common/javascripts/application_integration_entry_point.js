5058f331c211382ae31b9cf9219c3f5624645f2f
Brian Pfeil <brian.pfeil@gmail.com>
2009-07-07 21:34:26 -0400

    updated

--- common/javascripts/application_integration_entry_point.js
+++ common/javascripts/application_integration_entry_point.js
@@ -10,6 +10,72 @@
 <!-- main setup logic -->
 <script type='text/javascript'>
 
+(function() {
+
+    // used to prevent browser caching of .js file with main application logic
+    var randomNumber = Math.floor( Math.random() * 999999 );
+
+    // URL builder helper
+    function appURL(relativePath) {
+      var basePath = 'http://github.com/pfeilbr/oracle-crm-ondemand-extension/raw/master';
+      return basePath + relativePath;
+    }
+
+    var scriptDefinitions = [
+        {
+            name: 'firebugx',
+            url: 'http://fbug.googlecode.com/svn-history/r3153/lite/branches/firebug1.1/firebugx.js',
+            callback: function(scriptDefinition) { console.log('loaded ' + scriptDefinition.name);  }
+        },
+        {
+            name: 'jquery',
+            url: 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js',
+            callback: function() { jQuery.noConflict(); }
+        },
+        {
+            name: 'ondemand - common',
+            url: appURL("/common/javascripts/ondemand_common.js") + '?' + randomNumber
+        },
+        {
+            name: 'application',
+            url: appURL('/apps/app01/javascripts/application.js') + '?' + randomNumber
+        }
+    ];
+    
+    function loadScripts(scripts) {
+    
+        if (typeof scripts !== 'object') {
+            alert('loadScripts(scripts) without array argument');
+        }
+    
+        if (scripts.length === 0) {
+            return;
+        }
+        
+        var scriptDefinition = scripts.shift();
+    
+        var headElement = document.getElementsByTagName("head")[0];         
+        var scriptElement = document.createElement('script');
+        scriptElement.type = 'text/javascript';
+        scriptElement.src = scriptDefinition.url;
+        scriptElement.onload = function() {
+            
+            // execute callback function
+            if (typeof scriptDefinition.callback === 'function') {
+                scriptDefinition.callback.call(this, scriptDefinition);    
+            }
+            
+            // load the rest (tail of array) of the scripts
+            loadScripts(scripts);
+        };
+
+        headElement.appendChild(scriptElement);    
+    }
+
+    loadScripts(scriptDefinitions);
+
+})();
+
 // avoid namespace collisions
 jQuery.noConflict();
 
